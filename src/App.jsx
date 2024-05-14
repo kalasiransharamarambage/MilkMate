7626b6dadb536d826ca88e34803b0d45a7d28c05
DESKTOP-K6IHCUD\Sahan_Kumarage <kavishkasahandj@gmail.com>
2024-05-14 10:06:30 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Layout from '../src/Layout/layout';
-import Dashboard from './Pages/DashBoard/Dashboard';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "../src/Layout/layout";
+import Dashboard from "./Pages/DashBoard/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
-        <Route path="home" element={<Dashboard />} />
+          <Route path="/home" element={<Dashboard />} />
         </Route>
       </Routes>
     </Router>

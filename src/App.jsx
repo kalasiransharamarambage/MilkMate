6d9b89a0deb619abb04a657414388c80714c19a0
kalasiransharamarambage <149425770+kalasiransharamarambage@users.noreply.github.com>
2024-05-16 02:27:11 +0530

    form1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "../src/Layout/layout";
-import Dashboard from "./Pages/DashBoard/";
+import Dashboard from "./Pages/Dashboard-OM/Dashboard";
 import OrderForm from "./Pages/OrderForm-OM/OrderForm";
 
 function App() {
--- src/Pages/OrderForm-OM/OrderForm.jsx
+++ src/Pages/OrderForm-OM/OrderForm.jsx
@@ -1,9 +1,30 @@
+import { Col, Image, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Delete from "../../assets/Icon/delete.png";
+import Edit from "../../assets/Icon/pencil.png";
+
 
 function BasicExample() {
   return (
     <Form>
+
+<Form.Group className="mb-3" controlId="formBasicEmail">
+      <Row>
+       <Col xs={2} style={{display:"flex",justifyContent:"end" } }> 
+        <Form.Label>Full Name :</Form.Label>
+        </Col>
+        <Col>
+        <Form.Control type="text " placeholder="Enter your name" />
+        </Col>
+        <Col xs={2} display={"flex"} justifyContent = {"end"} style={{padding:"20px"}}>
+        <Image src={Delete } rounded style={{width:"30px",height:"30px"}}/>
+        <Image src={Edit } rounded style={{width:"25px",height:"25px"}}/>
+        
+        </Col>
+        </Row>
+      </Form.Group>
+     
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
@@ -12,15 +33,36 @@ function BasicExample() {
         </Form.Text>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Phone Number</Form.Label>
+        <Form.Control type="password" placeholder="Enter Contact" />
+        
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>District</Form.Label>
+        <Form.Control type="text " placeholder="Enter District" />
+        
       </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Delivery Address </Form.Label>
+        <Form.Control type="text" placeholder="Enter Address" />
+        
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Quantity</Form.Label>
+        <Form.Control type="text " placeholder="Enter Quantity" />
+        
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Pack Size</Form.Label>
+        <Form.Control type="text " placeholder="Enter packsize" />
+        
+      </Form.Group>
+     
+
+     <Button variant="outline-primary" type="submit">
+        Check Out
       </Button>
     </Form>
   );

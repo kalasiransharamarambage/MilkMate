58b7ca8970a39f1a7a8400170007c513cf0709ec
kalasiransharamarambage <149425770+kalasiransharamarambage@users.noreply.github.com>
2024-05-16 15:13:23 +0530

    Card

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "../src/Layout/layout";
 import Dashboard from "./Pages/Dashboard-OM/Dashboard";
 import OrderForm from "./Pages/OrderForm-OM/OrderForm";
+import CardDetails from "./Pages/CardDetails-OM/CardDetails";
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route path="/home" element={<Dashboard />} />
           <Route path="/orderForm" element={<OrderForm />} />
+          <Route path="/carddetails" element={<CardDetails />} />
         </Route>
       </Routes>
     </Router>

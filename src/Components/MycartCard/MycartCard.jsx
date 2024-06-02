f4503aed6edcda807e0fe9b1124de6325b7a0f7e
DESKTOP-K6IHCUD\Sahan_Kumarage <kavishkasahandj@gmail.com>
2024-06-02 17:46:11 +0530

    updated

--- src/Components/MycartCard/MycartCard.jsx
+++ src/Components/MycartCard/MycartCard.jsx
@@ -4,33 +4,33 @@ import Yogurt from "../../assets/Img/yogurt.jpg";
 import Curd from "../../assets/Img/yogurt.jpg";
 
 
-function MycartCard() {
+function MycartCard({name, image, price, qty, total, remove}) {
   return (
     <>
-      <Container fluid>
-        <Row style={{padding:"20px", backgroundColor:"#9AAFE4", borderRadius:"20px"}}>
+      <Container fluid >
+        <Row className="mt-3" style={{padding:"20px", backgroundColor:"#9AAFE4", borderRadius:"20px"}}>
           <Col style={{ display: "flex" }}>
             <Col xs={2} style={{display:"flex", justifyContent:"center"}}>
               <Image
-                src={Yogurt}
+                src={image}
                 style={{ width: "110px", borderRadius: "10px" }}
               />
             </Col>
             <Col xs={2} style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-                <h6>name</h6>
+                <h6>{name}</h6>
               <h3>Yogurt</h3>
             </Col>
             <Col xs={2} style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-              <h3>Price</h3>
+              <h3>{price}</h3>
             </Col>
             <Col xs={2} style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-              <h3>Qty</h3>
+              <h3>{qty}</h3>
             </Col>
             <Col xs={2} style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-              <h3>Total</h3>
+              <h3>{total}</h3>
             </Col>
             <Col xs={2} style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-              <h3>Delete</h3>
+              <h3>{remove}</h3>
             </Col>
           </Col>
         </Row>
--- src/Pages/MyCart-OM/mycart.jsx
+++ src/Pages/MyCart-OM/mycart.jsx
@@ -3,20 +3,41 @@ import CartCard from "../../Components/MycartCard/MycartCard";
 import { Col, Container, Row } from "react-bootstrap";
 
 function mycart() {
+  const SAMPLE_DATA =[
+    {
+      name:"Sahan",
+      price:100,
+      qty:1,
+      total:100,
+
+    },
+    {
+      name:"kVINDU",
+      price:100,
+      qty:1,
+      total:100,
+
+    },
+    {
+      name:"HASINDI",
+      price:100,
+      qty:1,
+      total:100,
+
+    },
+  ]
   return (
     <Container fluid>
       <Row>
-        <Col xs={8} >
-          <CartCard />
+        <Col xs={8}>
+        {SAMPLE_DATA.map((item) => (
+        <CartCard key={item.id} name={item.name} price={item.price} qty={item.qty} total={item.total} />
+      ))}
         </Col>
 
         <Col xs={4}></Col>
       </Row>
-      
-      
-      
     </Container>
-    
   );
 }
 

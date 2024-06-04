657dffa17840037018c31287542ac93207d4da8f
GayashiChathumini <gayashichathumini2001@gmail.com>
2024-06-04 09:12:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "../src/Layout/layout";
 import Dashboard from "./Pages/DashBoard/Dashboard";
+import Delivery1 from "./Pages/Delivery1/Delivery1";
+
 import Delivery2 from "./Pages/Delivery2/Delivery2";
 import Delivery3 from "./Pages/Delivery3/Delivery3";
 import Delivery4 from "./Pages/Delivery4/Delivery4";
@@ -13,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/home" element={<Dashboard />} />
+          <Route path="/delivery1" element={<Delivery1 />} />
           <Route path="/delivery2" element={<Delivery2 />} />
           <Route path="/delivery3" element={<Delivery3 />} />
           <Route path="/delivery4" element={<Delivery4 />} />

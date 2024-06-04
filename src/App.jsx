11c45223a0adbbcf62f0b6249e2d9f3cb9c70342
Tharushika525 <hirunit770@gmail.com>
2024-06-04 19:41:13 +0530

    Add App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "../src/Layout/layout";
 import Dashboard from "./Pages/DashBoard/Dashboard";
+import MainForm from "./Pages/Construction/MainForm";
+import CowParent from "./Pages/Construction/CowParent";
+import GoatParent from "./Pages/Construction/GoatParent";
+import CChildDisplay from "./Pages/Construction/CChildDisplay";
+import CParentDisplay from "./Pages/Construction/CParentDisplay";
+import GParentDisplay from "./Pages/Construction/GParentDisplay";
+import GChildDisplay from "./Pages/Construction/GChildDisplay";
+
 
 function App() {
   return (
@@ -9,6 +17,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/home" element={<Dashboard />} />
+          <Route path="/mainform" element= {<MainForm/>}/> 
+        <Route path="/cowparent" element= {<CowParent/>}/> 
+        <Route path="/goatparent" element= {<GoatParent/>}/> 
+        <Route path="/cchilddisplay" element= {<CChildDisplay/>}/> 
+        <Route path="/cparentdisplay" element= {<CParentDisplay/>}/>
+        <Route path="/gchilddisplay" element= {<GChildDisplay/>}/> 
+        <Route path="/gparentdisplay" element= {<GParentDisplay/>}/>    
+        
+
         </Route>
       </Routes>
     </Router>
